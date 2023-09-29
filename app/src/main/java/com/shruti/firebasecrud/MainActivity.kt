05ceec4b32fb991c8e0fd2d830d66a0f2b00463b
Shruti sharma <105702669+48Shruti@users.noreply.github.com>
2023-09-29 17:43:54 +0530

    update button using database

--- app/src/main/java/com/shruti/firebasecrud/MainActivity.kt
+++ app/src/main/java/com/shruti/firebasecrud/MainActivity.kt
@@ -4,7 +4,11 @@ import android.app.Dialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.firestore.ktx.toObject
+import com.google.firebase.ktx.Firebase
 import com.shruti.firebasecrud.databinding.ActivityMainBinding
 import com.shruti.firebasecrud.databinding.ItemLayoutBinding
 
@@ -12,6 +16,7 @@ class MainActivity : AppCompatActivity(),NotesInterface {
     var binding : ActivityMainBinding ?= null
     lateinit var adapter: RecyclerAdapter
     lateinit var layoutManager: LinearLayoutManager
+    var firestore = Firebase.firestore
     var item  = arrayListOf<NotesDataClass>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,6 +26,7 @@ class MainActivity : AppCompatActivity(),NotesInterface {
         binding?.recyceler?.layoutManager = layoutManager
         adapter = RecyclerAdapter(item, this)
         binding?.recyceler?.adapter = adapter
+        getCollectionsData()
 //        throw RuntimeException("Test Crash")
         binding?.fab?.setOnClickListener {
             var dialog = Dialog(this)
@@ -36,12 +42,28 @@ class MainActivity : AppCompatActivity(),NotesInterface {
                 } else if (dialogBinding.etdescription.text.isNullOrEmpty()) {
                     dialogBinding.etdescription.error = "Enter description"
                 } else {
-                    item.add(
+//                    item.add(
+//                        NotesDataClass(
+//                            title = dialogBinding.ettitle.text.toString(),
+//                            descriptions = dialogBinding.etdescription.text.toString()
+//                        )
+//                    )
+                    firestore.collection("Users").add(
                         NotesDataClass(
                             title = dialogBinding.ettitle.text.toString(),
                             descriptions = dialogBinding.etdescription.text.toString()
                         )
                     )
+                        .addOnSuccessListener {
+                            Toast.makeText(this,"DAta Added",Toast.LENGTH_SHORT).show()
+                            getCollectionsData()
+                        }
+                        .addOnFailureListener{
+                            Toast.makeText(this,"Data fail",Toast.LENGTH_SHORT).show()
+                        }
+                        .addOnCanceledListener {
+                            Toast.makeText(this," Data Cancel",Toast.LENGTH_SHORT).show()
+                        }
                     dialog.dismiss()
                 }
             }
@@ -60,29 +82,58 @@ class MainActivity : AppCompatActivity(),NotesInterface {
     override fun update(notesDataClass: NotesDataClass, position: Int) {
         var dialog = Dialog(this)
         var dialogBinding = ItemLayoutBinding.inflate(layoutInflater)
-        dialog.window?.setLayout(
-            ViewGroup.LayoutParams.MATCH_PARENT,
-            ViewGroup.LayoutParams.MATCH_PARENT
-        )
+
         dialog.setContentView(dialogBinding.root)
+    dialog.window?.setLayout(
+        ViewGroup.LayoutParams.MATCH_PARENT,
+        ViewGroup.LayoutParams.WRAP_CONTENT
+    )
+    dialogBinding.ettitle.setText(notesDataClass.title)
+    dialogBinding.etdescription.setText(notesDataClass.descriptions)
         dialogBinding.save.setOnClickListener {
             if (dialogBinding.ettitle.text.isNullOrEmpty()) {
                 dialogBinding.ettitle.error = "Enter title"
             } else if (dialogBinding.etdescription.text.isNullOrEmpty()) {
                 dialogBinding.etdescription.error = "Enter description"
             } else {
-                item.set(position,
+                var updateNotes =
                     NotesDataClass(
                         title = dialogBinding.ettitle.text.toString(),
                         descriptions = dialogBinding.etdescription.text.toString()
                     )
-                )
+                firestore.collection("Users").document(notesDataClass.id?:"")
+                    .set(updateNotes)
+                    .addOnSuccessListener {
+                        Toast.makeText(this,"Data Suceess",Toast.LENGTH_SHORT).show()
+                        getCollectionsData()
+                    }
+                    .addOnCanceledListener {
+                        Toast.makeText(this,"Data Cancel",Toast.LENGTH_SHORT).show()
+                    }
+                    .addOnFailureListener{
+                        Toast.makeText(this,"Data Fail",Toast.LENGTH_SHORT).show()
+                    }
+                adapter.notifyDataSetChanged()
                 dialog.dismiss()
             }
         }
+    dialog.show()
     }
 
     override fun delete(notesDataClass: NotesDataClass, position: Int) {
        item.removeAt(position)
+        adapter.notifyDataSetChanged()
+    }
+    private fun getCollectionsData(){
+        item.clear()
+        firestore.collection("Users").get()
+            .addOnSuccessListener {
+                for (items in it.documents){
+                    var firestore = items.toObject(NotesDataClass::class.java)?: NotesDataClass()
+                    firestore.id =  items.id
+                    item.add(firestore)
+                }
+                adapter.notifyDataSetChanged()
+            }
     }
 }
--- app/src/main/java/com/shruti/firebasecrud/NotesDataClass.kt
+++ app/src/main/java/com/shruti/firebasecrud/NotesDataClass.kt
@@ -1,4 +1,5 @@
 package com.shruti.firebasecrud
 
-data class NotesDataClass(var title : String,
-var descriptions : String)
+data class NotesDataClass(var title : String ?= null,
+var descriptions : String ?= null,
+var id : String ?= null)
